8b5d6f717b02f138589d995a58641bb154558ab8
mdmustakim9752-byte <mdmustakim9752@gmail.com>
2026-02-18 20:05:04 +0530

    Create password_loop.py

---
+++ password_loop.py
@@ -0,0 +1,11 @@
+name = input("apna name batao.   ")
+
+while True:
+    password = input("password dalo.   ")
+
+    if password == "1234":
+     print(f"Bahut sahi... {name} 😍 apne perfect password dala")
+     break
+
+    else:
+     print(f"Galat password hai... {name} kya aap ek baar aur try kar sakte ho?😞  ")
